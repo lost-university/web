4a128608e3a64bee7387e2038eb08ea7196e78dc
Jeremy Stucki <dev@jeremystucki.ch>
2024-01-29 17:36:56 +0100

    Bring GitHub icon back

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import '../styles/main.scss';
 // import Font Awesome as Vue Component: Dynamic Icon Change does not work without it.
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library, type IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import App from './App.vue';
 import router from './router';
 
 library.add(faChevronUp as IconDefinition);
 library.add(faChevronDown as IconDefinition);
+library.add(faGithub as IconDefinition);
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
