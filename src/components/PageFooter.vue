0368f62ff151af274d477105a886753658a36587
Stefanie Jäger <stefijaeger@gmail.com>
2025-01-25 11:22:58 +0100

    Add footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -26,6 +26,16 @@
       />
     </a>
   </footer>
+  <div class="bg-[#8c195f] text-white text-center py-4">
+        <span class="font-bold">
+      Die Webseite wurde kürzlich im Rahmen einer SA überarbeitet und hat jetzt ein paar neue Features
+    </span>
+    <br />
+    <span class="text-sm">
+      Falls etwas nicht funktionieren sollte, eröffne bitte ein Issue <a href="https://github.com/lost-university/web/issues/new/choose"><u>auf GitHub.</u></a>
+      Die vorherige Version ist unter <a href="https://old.lost.university"><u>old.lost.university</u></a> weiterhin für eine begrenzte Zeit verfügbar.
+    </span>
+  </div>
 </template>
 
 <script lang="ts">
