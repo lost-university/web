cfe5cb4d4937c2cd2d61560f4037ce23ac6166b4
Jeremy Stucki <dev@jeremystucki.ch>
2025-01-29 15:34:45 +0100

    Update data base-url

--- src/helpers/store.ts
+++ src/helpers/store.ts
@@ -3,7 +3,7 @@ import { AccreditedModule, Category, Focus, Module, Semester } from './types';
 import { SemesterInfo } from './semester-info';
 import { getColorClassForCategoryId } from '../helpers/color-helper';
 
-const BASE_URL = 'https://raw.githubusercontent.com/StefanieJaeger/lost-university-data/SJ/after-sa/data';
+const BASE_URL = 'https://raw.githubusercontent.com/lost-university/data/5.0/data';
 const ROUTE_MODULES = '/modules.json';
 const ROUTE_CATEGORIES = '/categories.json';
 const ROUTE_FOCUSES = '/focuses.json';
