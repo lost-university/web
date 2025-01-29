8efc3de3d51c89a012fddf39e1d8ff43e93fea54
Jeremy Stucki <dev@jeremystucki.ch>
2025-01-29 17:18:50 +0100

    Bump node version to 20

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
