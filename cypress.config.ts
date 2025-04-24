7a4e668d458cd6e1f773badde94b364c8fdb5867
matteo.mahler <matteo.mahler@ost.ch>
2025-04-24 21:24:20 +0200

    use different ui version for system tests and fix firefox error

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,16 @@ export default defineConfig({
         process.env.CLERK_PUBLISHABLE_KEY = config.env.CLERK_PUBLISHABLE_KEY
       }
 
+      // Firefox has stricter cookie policies than the other browsers. This settings is only needed
+      // because we are not using https in the tests which has an impact on clerk cookies.
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'firefox') {
+          launchOptions.preferences['network.cookie.sameSite.noneRequiresSecure'] = false;
+          launchOptions.preferences['network.cookie.sameSite.laxByDefault'] = false;
+          return launchOptions;
+        }
+      });
+
       return clerkSetup({ config })
     },
   },
