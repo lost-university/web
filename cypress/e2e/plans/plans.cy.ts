2857fc51be83995b92a48073a33710f3b4e4c13f
martin.arendar <martin.arendar@gmx.ch>
2025-05-12 18:25:36 +0200

    Refactor test case to create 2 plans with a timestamp as its name and then check if the two names exist in the dropdown

--- cypress/e2e/plans/plans.cy.ts
+++ cypress/e2e/plans/plans.cy.ts
@@ -8,9 +8,18 @@ describe('multiple plans', () => {
       cy.visit('/');
       cy.clerkSignIn({ strategy: 'email_code', identifier: 'user1+clerk_test@lost.university' })
 
-      //cy.get("[data-cy='SavedPlans-Dropdown-Button']").trigger('onmouseover')
-      //cy.get("[data-cy='SavedPlans-Dropdown-Button']").trigger('mouseenter')
-      //cy.get("[data-cy='SavedPlans-Dropdown-Button']").invoke('trigger', 'contextmenu')
+
+      for (let i = 0; i < 2; i++) {
+        const timestamp = `${new Date().getTime()}-${i}`;
+
+        cy.get("[data-cy='SavedPlans-Dropdown-Button']").realHover();
+        cy.get("[data-cy='SavePlan-Button']").click();
+        cy.get("[data-cy='SavePlan-Name']").type(`Test Plan ${timestamp}`);
+        cy.get("[data-cy='SavePlan-Submit']").click();
+
+        cy.get("[data-cy=SavedPlans-List-Item").filter(`:contains("Test Plan ${timestamp}")`).should('exist');
+      }
+    });
       cy.get("[data-cy='SavedPlans-Dropdown-Button']").realHover();
       cy.get("[data-cy='SavePlan-Button']").click();
       cy.get("[data-cy='SavePlan-Name']").type('Test Plan 1');
