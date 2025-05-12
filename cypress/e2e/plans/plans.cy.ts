66388b498e49da7b96a2224f29bb0388ce61a7fa
martin.arendar <martin.arendar@gmx.ch>
2025-05-12 18:26:19 +0200

    Create testcase that creates a new plan, deletes it and then checks that the plan name is not in the dropdown anymore

--- cypress/e2e/plans/plans.cy.ts
+++ cypress/e2e/plans/plans.cy.ts
@@ -20,10 +20,25 @@ describe('multiple plans', () => {
         cy.get("[data-cy=SavedPlans-List-Item").filter(`:contains("Test Plan ${timestamp}")`).should('exist');
       }
     });
+    it('delete 1 plan', () => {
+      setupClerkTestingToken()
+
+      cy.visit('/');
+      cy.clerkSignIn({ strategy: 'email_code', identifier: 'user1+clerk_test@lost.university' })
+
       cy.get("[data-cy='SavedPlans-Dropdown-Button']").realHover();
       cy.get("[data-cy='SavePlan-Button']").click();
-      cy.get("[data-cy='SavePlan-Name']").type('Test Plan 1');
+      cy.get("[data-cy='SavePlan-Name']").type(`Test Plan: To Delete`);
       cy.get("[data-cy='SavePlan-Submit']").click();
-    })
+
+      cy.get("[data-cy=SavedPlans-List-Item").filter(`:contains("Test Plan: To Delete")`)
+        .within(() => {
+          cy.get('[data-cy=SavedPlans-Delete-Button]')
+            .click()
+        })
+
+      cy.get("[data-cy='SavedPlans-Dropdown-Button']").realHover();
+      cy.contains("[data-cy='SavedPlans-List-Item']", "Test Plan: To Delete").should('not.exist');
+    });
   })
 })
