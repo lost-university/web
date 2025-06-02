e73809d74c6483f4eec11bbfcd455fd62d830e61
pheonix8 <61965711+pheonix8@users.noreply.github.com>
2025-06-02 11:28:10 +0200

    work on tests

--- src/components/__tests__/SavedPlanActionMenu.cy.ts
+++ src/components/__tests__/SavedPlanActionMenu.cy.ts
@@ -1,4 +1,5 @@
 import { mount } from 'cypress/vue';
+
 import SavedPlansActionMenu from '../SavedPlansActionMenu.vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -11,22 +12,22 @@ import {
 
 library.add(faTrash, faShareNodes, faCheck, faEllipsis);
 
-describe('SavedPlansActionMenu.vue', () => {
-  const fakePlan = {
-    id: 'plan-1',
-    name: 'Test Plan',
-    publicSlug: 'abc123',
-    content: '/plan/1'
-  };
+describe('SavedPlan Action Menu Component', () => {
+  beforeEach(() => {
+    const fakePlan = {
+      id: 'plan-1',
+      name: 'Test Plan',
+      publicSlug: 'abc123',
+      content: '/plan/1'
+    };
 
-  it('should render menu and trigger share and delete actions', () => {
     const onDelete = cy.spy().as('onDelete');
 
     mount(SavedPlansActionMenu, {
       props: {
         plan: fakePlan,
         menuPositionClass: 'top-0 left-0',
-        onDelete
+        onDelete: onDelete
       },
       global: {
         components: {
@@ -35,26 +36,27 @@ describe('SavedPlansActionMenu.vue', () => {
       }
     });
 
-    // Open the menu
+  });
+
+
+  it('Test Menu Items', () => {
+
+
+
     cy.get('[data-cy=menu-button]').click();
 
-    // Click the share button
     cy.get('[data-cy=share-button]').click();
 
-    // Check icon changes to checkmark
     cy.get('[data-cy=share-icon]')
       .should('have.class', 'text-green-600');
 
-    // Wait for the menu to close automatically
     cy.wait(1200);
 
-    // Open again
     cy.get('[data-cy=menu-button]').click();
 
     cy.get('[data-cy=share-icon]')
       .should('have.class', 'text-black');
 
-    // Click the delete button
     cy.get('[data-cy=delete-button]').click();
 
     cy.get('@onDelete').should('have.been.calledWith', 'plan-1');
