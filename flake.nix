a92c38abd318c7e52eedd55f3015399cdd727b2c
Jeremy Stucki <dev@jeremystucki.ch>
2025-11-15 16:26:05 +0100

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,11 +10,10 @@
       system:
       let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
-        pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell { nativeBuildInputs = [ pkgs.nodejs ]; };
-        formatter = pkgs-unstable.nixfmt-rfc-style;
+        formatter = pkgs.nixfmt-tree;
       }
     );
 }
