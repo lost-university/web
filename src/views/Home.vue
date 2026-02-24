1c503f36d2b0eaf4aa301347c7e77c7e50726118
TheJanzap <16736682+TheJanzap@users.noreply.github.com>
2026-02-24 11:39:58 +0100

    feat: Change meme image based on current month

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -122,8 +122,8 @@
     </article>
     <img
       class="lg:col-span-2 2xl:col-span-1 justify-self-center px-4"
-      src="/this_is_fine_summer.jpg"
-      alt="A twist on the well known 'this is fine' meme with a dog sitting at the beach with a drink."
+      :src="seasonalImage.src"
+      :alt="seasonalImage.alt"
     >
   </div>
 </template>
@@ -173,6 +173,25 @@ export default defineComponent({
     addingSemesterIsDisabled() {
       return this.enrichedSemesters.length >= SemesterInfo.maxNumberOfAllowedSemesters;
     },
+    seasonalImage() {
+      const currentMonth = new Date().getMonth() + 1;
+      if (currentMonth >= 6 && currentMonth <= 9) {
+        return {
+          src: '/this_is_fine_summer.jpg',
+          alt: "A twist on the well known 'this is fine' meme with a dog sitting at the beach with a drink."
+        };
+      }
+      else if (currentMonth >= 12 || currentMonth <= 2) {
+        return {
+          src: '/this_is_fine_winter.jpg',
+          alt: "Alternative version of the well known 'this is fine' meme with a dog in a room full of snow."
+        };
+      }
+      return {
+        src: '/this_is_fine.jpg',
+        alt: "The well known 'this is fine' meme with a dog in a room full of fire."
+      }
+    }
   },
   watch: {
     $route: {
